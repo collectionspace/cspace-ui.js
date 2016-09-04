1eb556d2703c773fc8c630144b04f5b8638ecdd8
Ray Lee <rhlee@berkeley.edu>
2016-09-04 00:14:01 -0700

    DRYD-35: Move navigation components into subdirectory.

--- src/components/navigation/NavLink.jsx
+++ src/components/navigation/NavLink.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-require('../styles/NavLink.css');
+require('../../styles/NavLink.css');
 
 export default (props) => (
   <Link {...props} className="NavLink" activeClassName="active" />
