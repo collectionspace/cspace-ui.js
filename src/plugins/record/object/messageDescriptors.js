5dc3a26926eb109550e82399fcfc944327e43e38
Ray Lee <rhlee@berkeley.edu>
2016-09-29 00:46:19 -0700

    DRYD-46,DRYD-47: Add descriptions to message definitions.

--- src/plugins/record/object/messageDescriptors.js
+++ src/plugins/record/object/messageDescriptors.js
@@ -3,115 +3,142 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   recordNameTitle: {
     id: 'recordNameTitle',
-    description: 'The name of the record, when used as a title.',
+    description: 'The name of the record when used as a title.',
     defaultMessage: 'Object',
   },
   objectNumber: {
     id: 'field.objectNumber.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Identification number',
   },
   numberOfObjects: {
     id: 'field.numberOfObjects.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Number of objects',
   },
   otherNumberList: {
     id: 'field.otherNumberList.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Other number',
   },
   numberValue: {
     id: 'field.numberValue.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Number',
   },
   numberType: {
     id: 'field.numberType.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Type',
   },
   responsibleDepartments: {
     id: 'field.responsibleDepartments.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Responsible department',
   },
   collection: {
     id: 'field.collection.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Collection',
   },
   recordStatus: {
     id: 'field.recordStatus.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Record status',
   },
   briefDescriptions: {
     id: 'field.briefDescriptions.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Brief description',
   },
   distinguishingFeatures: {
     id: 'field.distinguishingFeatures.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Distinguishing features',
   },
   comments: {
     id: 'field.comments.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Comment',
   },
   computedCurrentLocation: {
     id: 'field.computedCurrentLocation.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Computed current location',
   },
   titleGroupList: {
     id: 'field.titleGroupList.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Title',
   },
   title: {
     id: 'field.title.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Title',
   },
   titleLanguage: {
     id: 'field.titleLanguage.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Language',
   },
   titleType: {
     id: 'field.titleType.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Type',
   },
   titleTranslationSubGroupList: {
     id: 'field.titleTranslationSubGroupList.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Translation',
   },
   titleTranslation: {
     id: 'field.titleTranslation.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Translation',
   },
   titleTranslationLanguage: {
     id: 'field.titleTranslationLanguage.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Language',
   },
   objectNameList: {
     id: 'field.objectNameList.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Object name',
   },
   objectName: {
     id: 'field.objectName.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Name',
   },
   objectNameCurrency: {
     id: 'field.objectNameCurrency.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Currency',
   },
   objectNameLevel: {
     id: 'field.objectNameLevel.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Level',
   },
   objectNameSystem: {
     id: 'field.objectNameSystem.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'System',
   },
   objectNameType: {
     id: 'field.objectNameType.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Type',
   },
   objectNameLanguage: {
     id: 'field.objectNameLanguage.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Language',
   },
   objectNameNote: {
     id: 'field.objectNameNote.label',
+    description: 'A short field label used in the record editor form.',
     defaultMessage: 'Note',
   },
 });
