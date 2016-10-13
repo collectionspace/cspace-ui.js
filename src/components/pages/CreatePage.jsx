6fb4d2e9270f305094f4a5944b9fc3fb42f8fe27
Ray Lee <rhlee@berkeley.edu>
2016-10-13 00:09:29 -0700

    DRYD-72: Add links to Create New screen.

--- src/components/pages/CreatePage.jsx
+++ src/components/pages/CreatePage.jsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router';
 
-export default () => (
-  <h2>Create New</h2>
-);
+const CreatePage = (props, context) => {
+  const {
+    recordPlugins,
+  } = context;
+
+  const items = Object.keys(recordPlugins).map(recordType =>
+    <li key={recordType}>
+      <Link to={`record/${recordType}`}>
+        <FormattedMessage {...recordPlugins[recordType].messageDescriptors.recordNameTitle} />
+      </Link>
+    </li>
+  );
+
+  return (
+    <div>
+      <h2>Create New</h2>
+
+      <ul>
+        {items}
+      </ul>
+    </div>
+  );
+};
+
+CreatePage.contextTypes = {
+  recordPlugins: PropTypes.object,
+};
+
+export default CreatePage;
