4a334802a7d5ebb60d9c1aa253291bbf3b92f7ff
Yousuf Nejati <yousufnejati@gmail.com>
2017-06-29 06:01:49 -0700

    Add more Acquisition record type tests.

--- test/specs/plugins/recordTypes/acquisition/columns.spec.js
+++ test/specs/plugins/recordTypes/acquisition/columns.spec.js
@@ -6,4 +6,13 @@ describe('acquisition record columns', function suite() {
   it('should have the correct shape', function test() {
     columns.should.have.property('default').that.is.an('array');
   });
+
+  it('should have acquisition source column that is formatted as a refname display name', function test() {
+    const acquisitionSourceColumn = columns.default.find(column => column.name === 'acquisitionSource');
+
+    acquisitionSourceColumn.should.have.property('formatValue').that.is.a('function');
+
+    acquisitionSourceColumn.formatValue('urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(johndoe)\'John Doe\'').should
+      .equal('John Doe');
+  });
 });
