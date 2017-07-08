a40d7e03ef8949c080a61181ea052c1b34a52442
Yousuf Nejati <yousufnejati@gmail.com>
2017-07-08 08:03:20 -0700

    Add test to object exit column spec to increase coverage.

--- test/specs/plugins/recordTypes/objectexit/columns.spec.js
+++ test/specs/plugins/recordTypes/objectexit/columns.spec.js
@@ -6,4 +6,13 @@ describe('object exit record columns', function suite() {
   it('should have the correct shape', function test() {
     columns.should.have.property('default').that.is.an('array');
   });
+
+  it('should have current owner column that is formatted as a refname display name', function test() {
+  	const currentOwnerColumn = columns.default.find(column => column.name === 'currentOwner');
+
+    currentOwnerColumn.should.have.property('formatValue').that.is.a('function');
+
+    currentOwnerColumn.formatValue('urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(johndoe)\'John Doe\'').should
+      .equal('John Doe');
+  });
 });
