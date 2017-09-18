55f2705eb3841c39e16dec75fc8a16554d98893c
Richard Millet <remillet@yahoo.com>
2017-09-18 12:59:39 -0700

    DRYD-159: Adding missing test converage.

--- test/specs/plugins/recordTypes/pottag/columns.spec.js
+++ test/specs/plugins/recordTypes/pottag/columns.spec.js
@@ -15,4 +15,13 @@ describe('pot tag record columns', function suite() {
     familyColumn.formatValue('urn:cspace:botgarden.collectionspace.org:taxonomyauthority:name(taxon):item:name(PTFamily1501262583720)\'PTFamily\'').should
       .equal('PTFamily');
   });
+
+  it('should have taxonName column that is formatted as a refname display name', function test() {
+    const taxonNameColumn = columns.default.find(column => column.name === 'taxonName');
+
+    taxonNameColumn.should.have.property('formatValue').that.is.a('function');
+
+    taxonNameColumn.formatValue('urn:cspace:botgarden.collectionspace.org:taxonomyauthority:name(taxon):item:name(TaxonName1501262583720)\'TaxonName\'').should
+      .equal('TaxonName');
+  });
 });
