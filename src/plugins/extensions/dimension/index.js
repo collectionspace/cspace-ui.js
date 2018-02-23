f5678337fd36f759c1dfd99cd6d2f1dfa2d25930
Ray Lee <rhlee@berkeley.edu>
2018-02-22 17:13:00 -0800

    NOJIRA: Move measurementUnits option list config into dimension extension.

--- src/plugins/extensions/dimension/index.js
+++ src/plugins/extensions/dimension/index.js
@@ -1,7 +1,9 @@
 import fields from './fields';
 import form from './form';
+import optionLists from './optionLists';
 
 export default () => pluginContext => ({
+  optionLists,
   extensions: {
     dimension: {
       fields: fields(pluginContext),
---
+++ src/plugins/extensions/dimension/optionLists.js
@@ -0,0 +1,80 @@
+import { defineMessages } from 'react-intl';
+
+// FIXME: Plugins shouldn't have to import react-intl, since this unnecessarily increases the size
+// when built as a standalone package. Instead, defineMessages should be supplied in the
+// pluginContext. But this means that messages won't be extracted by the Babel plugin, since it
+// only operates on files that import react-intl.
+
+export default {
+  measurementUnits: {
+    values: [
+      'carats',
+      'centimeters',
+      'cubic-centimeters',
+      'feet',
+      'inches',
+      'kilograms',
+      'liters',
+      'millimeters',
+      'meters',
+      'minutes',
+      'pixels',
+      'square-feet',
+      'stories',
+    ],
+    messages: defineMessages({
+      carats: {
+        id: 'option.measurementUnits.carats',
+        defaultMessage: 'carats',
+      },
+      centimeters: {
+        id: 'option.measurementUnits.centimeters',
+        defaultMessage: 'centimeters',
+      },
+      'cubic-centimeters': {
+        id: 'option.measurementUnits.cubic-centimeters',
+        defaultMessage: 'cubic centimeters',
+      },
+      feet: {
+        id: 'option.measurementUnits.feet',
+        defaultMessage: 'feet',
+      },
+      inches: {
+        id: 'option.measurementUnits.inches',
+        defaultMessage: 'inches',
+      },
+      kilograms: {
+        id: 'option.measurementUnits.kilograms',
+        defaultMessage: 'kilograms',
+      },
+      liters: {
+        id: 'option.measurementUnits.liters',
+        defaultMessage: 'liters',
+      },
+      millimeters: {
+        id: 'option.measurementUnits.millimeters',
+        defaultMessage: 'millimeters',
+      },
+      meters: {
+        id: 'option.measurementUnits.meters',
+        defaultMessage: 'meters',
+      },
+      minutes: {
+        id: 'option.measurementUnits.minutes',
+        defaultMessage: 'minutes',
+      },
+      pixels: {
+        id: 'option.measurementUnits.pixels',
+        defaultMessage: 'pixels',
+      },
+      'square-feet': {
+        id: 'option.measurementUnits.square-feet',
+        defaultMessage: 'square feet',
+      },
+      stories: {
+        id: 'option.measurementUnits.stories',
+        defaultMessage: 'stories',
+      },
+    }),
+  },
+};
