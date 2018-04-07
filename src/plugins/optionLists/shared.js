f2a9d4d4d09f0c4254777e7d9e93ad2001b5c668
Ray Lee <rhlee@berkeley.edu>
2018-04-07 00:14:13 -0700

    NOJIRA: Remove some FIXME comments.

--- src/plugins/optionLists/shared.js
+++ src/plugins/optionLists/shared.js
@@ -1,10 +1,5 @@
 import { defineMessages } from 'react-intl';
 
-// FIXME: Plugins shouldn't have to import react-intl, since this unnecessarily increases the size
-// when built as a standalone package. Instead, defineMessages should be supplied in the
-// configContext. But this means that messages won't be extracted by the Babel plugin, since it
-// only operates on files that import react-intl.
-
 /**
  * This plugin provides option lists that are shared by multiple record types. Option lists that
  * are used by only one record type are defined in the plugin for that record type.
--- src/plugins/recordTypes/valuation/optionLists.js
+++ src/plugins/recordTypes/valuation/optionLists.js
@@ -1,10 +1,5 @@
 import { defineMessages } from 'react-intl';
 
-// FIXME: Plugins shouldn't have to import react-intl, since this unnecessarily increases the size
-// when built as a standalone package. Instead, defineMessages should be supplied in the
-// configContext. But this means that messages won't be extracted by the Babel plugin, since it
-// only operates on files that import react-intl.
-
 export default {
   valueTypes: {
     values: [
