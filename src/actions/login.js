7465529f572641ec9f52b5f43f46dae5dccc9fd1
Ray Lee <rhlee@berkeley.edu>
2018-08-06 01:26:38 -0700

    NOJIRA: Fix authorities not being loaded after login.

--- src/actions/login.js
+++ src/actions/login.js
@@ -99,7 +99,7 @@ export const login = (config, username, password) => (dispatch, getState) => {
 
   return dispatch(renewAuth(config, username, password))
     .then(() => dispatch(loadPrefs(config, username)))
-    .then(() => dispatch(readAuthVocabs()))
+    .then(() => dispatch(readAuthVocabs(config)))
     .then(() => dispatch({
       type: LOGIN_FULFILLED,
       meta: {
