788fc9f59eccb79fc5e0515aba39ce0849083d43
Ray Lee <ray.lee@lyrasis.org>
2019-05-18 22:21:54 -0700

    NOJIRA: Delete report/batch parameter forms that were accidentally committed.

--- src/plugins/invocables/batch/index.js
+++ src/plugins/invocables/batch/index.js
@@ -1,5 +1,2 @@
-import updateObjectAndCrateLocation from './Update Object And Crate Location';
-
 export default [
-  updateObjectAndCrateLocation,
 ];
--- src/plugins/invocables/report/index.js
+++ src/plugins/invocables/report/index.js
@@ -1,9 +1,2 @@
-import coreAcquisition from './coreAcquisition';
-import SystematicInventory from './SystematicInventory';
-import SystematicInventoryUI from './SystematicInventoryUI';
-
 export default [
-  coreAcquisition,
-  SystematicInventory,
-  SystematicInventoryUI,
 ];
