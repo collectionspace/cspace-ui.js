545b82cc9d0f3908f273016573c49fe53b8be31a
cesarvh <cesarv.h@berkeley.edu>
2020-03-18 11:26:26 -0700

    CC-1058/CC-1102: Remove ucbnh and pahma specific fields

--- src/plugins/invocables/batch/org.collectionspace.services.batch.nuxeo.BulkObjectEditBatchJob/fields.js
+++ src/plugins/invocables/batch/org.collectionspace.services.batch.nuxeo.BulkObjectEditBatchJob/fields.js
@@ -6,7 +6,6 @@ export default (configContext) => {
     AutocompleteInput,
     TextInput,
     OptionPickerInput,
-    
   } = configContext.inputComponents;
 
   const {
@@ -408,20 +407,6 @@ export default (configContext) => {
             },
           },
         },
-      },      
-      //  pahma
-      inventoryCount: {
-        [config]: {
-          messages: defineMessages({
-            name: {
-              id: 'field.BulkObjectEditBatchJob.inventoryCount.name',
-              defaultMessage: 'Count',
-            },
-          }),
-          view: {
-            type: TextInput,
-          },
-        },
       },
       // core
       assocPeopleGroup: {
@@ -454,45 +439,6 @@ export default (configContext) => {
             }),
             view: {
               type: TextInput,
-            },
-          },
-        },
-      },
-      // pahma
-      pahmaFieldLocVerbatim: {
-        [config]: {
-          messages: defineMessages({
-            name: {
-              id: 'field.BulkObjectEditBatchJob.pahmaFieldLocVerbatim.name',
-              defaultMessage: 'Field collection place (verbatim)',
-            },
-          }),
-          view: {
-            type: TextInput,
-          },
-        },
-      },
-      // pahma
-      pahmaEthnographicFileCodeList: {
-        [config]: {
-          view: {
-            type: CompoundInput,
-          },
-        },
-        pahmaEthnographicFileCode: {
-          [config]: {
-            messages: defineMessages({
-              name: {
-                id: 'field.BulkObjectEditBatchJob.pahmaEthnographicFileCodeList.name',
-                defaultMessage: 'Ethnographic file code',
-              },
-            }),
-            repeating: true,
-            view: {
-              type: AutocompleteInput,
-              props: {
-                source: 'concept/ethusecode',
-              },
             },
           },
         },
--- src/plugins/invocables/batch/org.collectionspace.services.batch.nuxeo.BulkObjectEditBatchJob/forms/default.jsx
+++ src/plugins/invocables/batch/org.collectionspace.services.batch.nuxeo.BulkObjectEditBatchJob/forms/default.jsx
@@ -12,95 +12,80 @@ const template = (configContext) => {
   const {
     Col,
     Cols,
-    Panel,
   } = configContext.layoutComponents;
 
   return (
     <Field name="params">
+      <Cols>
+        <Col>
+          <Field name="numberOfObjects" />
 
-        <Cols>
-          <Col>
-            <Field name="inventoryCount"/>
-            <Field name="numberOfObjects"/>
-            
-            <Field name="otherNumberList">
-              <Field name="otherNumber">
-                <Field name="numberValue" />
-                <Field name="numberType"/>    
-              </Field>
+          <Field name="otherNumberList">
+            <Field name="otherNumber">
+              <Field name="numberValue" />
+              <Field name="numberType" />
             </Field>
+          </Field>
 
-            {/* Object type  */}
-            <Field name="fieldCollectionDate" />
-            <Field name="materialGroup">
-              <Field name="material"/>
-            </Field>
+          <Field name="fieldCollectionDate" />
+          <Field name="materialGroup">
+            <Field name="material" />
+          </Field>
 
-            <Field name="fieldCollectionPlace" /> 
+          <Field name="fieldCollectionPlace" />
 
-            <Field name="objectNameGroup">
-              <Field name="objectName"/>
-            </Field>
-          </Col>
+          <Field name="objectNameGroup">
+            <Field name="objectName" />
+          </Field>
+        </Col>
 
-          <Col>
-            <Field name="assocPeopleGroup">
-              <Field name="assocPeople" />
-            </Field>
+        <Col>
+          <Field name="assocPeopleGroup">
+            <Field name="assocPeople" />
+          </Field>
 
-            <Field name="objectProductionPersonGroup">
-              <Field name="objectProductionPerson" />
-            </Field>
+          <Field name="objectProductionPersonGroup">
+            <Field name="objectProductionPerson" />
+          </Field>
 
-            <Field name="objectProductionDate" />
-            <Field name="taxonomicIdentGroup">
-                <Field name="taxon"/>
-            </Field>   
+          <Field name="objectProductionDate" />
 
-            <Field name="objectProductionPlaceGroup">
-              <Field name="objectProductionPlace" />
-            </Field>
+          <Field name="objectProductionPlaceGroup">
+            <Field name="objectProductionPlace" />
+          </Field>
 
-            <Field name="responsibleDepartments">
-              <Field name="responsibleDepartment"/>
-            </Field>
-          </Col>
+          <Field name="responsibleDepartments">
+            <Field name="responsibleDepartment" />
+          </Field>
+        </Col>
 
-          <Col>
-            <Field name="pahmaEthnographicFileCodeList">
-              <Field name="pahmaEthnographicFileCode" />
-            </Field>
+        <Col>
+          <Field name="fieldCollectors">
+            <Field name="fieldCollector" />
+          </Field>
 
-            <Field name="fieldCollectors">
-              <Field name="fieldCollector" />
-            </Field>
+          <Field name="objectStatusList">
+            <Field name="objectStatus" />
+          </Field>
 
-            <Field name="objectStatusList">
-              <Field name="objectStatus"/>     
-            </Field>
+          <Field name="contentDateGroup" />
+          <Field name="contentPlaces">
+            <Field name="contentPlace" />
+          </Field>
+        </Col>
+      </Cols>
 
-            <Field name="contentDateGroup" />
-            <Field name="pahmaFieldLocVerbatim" />  
-
-            <Field name="contentPlaces">
-              <Field name="contentPlace" />
-            </Field>
-          </Col>
-
-        </Cols>
-
-        <Field name="briefDescriptions">
-          <Field name="briefDescription"/>       
-        </Field>
-
-  </Field>
+      <Field name="briefDescriptions">
+        <Field name="briefDescription" />
+      </Field>
+    </Field>
   );
 };
 
 export default configContext => ({
   messages: defineMessages({
     name: {
-      id: 'form.report.Bulk Edit Batch Job.default.name',
+      id: 'form.report.Bulk Object Edit Batch Job.default.name',
       defaultMessage: 'Standard Template',
     },
   }),
