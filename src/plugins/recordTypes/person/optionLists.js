ac624a8bb9d534a611d4d3593d873bb5dd6cda85
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-10-28 14:51:35 -0400

    Add personTitles values; alphabetize list

--- src/plugins/recordTypes/person/optionLists.js
+++ src/plugins/recordTypes/person/optionLists.js
@@ -71,20 +71,108 @@ export default {
   },
   personTitles: {
     values: [
+      'Admiral',
+      'Baron',
+      'Baroness',
+      'Captain',
+      'Commander',
+      'Commodore',
+      'Count',
+      'Countess',
+      'Dame',
+      'Dr',
+      'General',
+      'Governor',
+      'Honorable',
+      'Judge',
+      'King', 
+      'Lady',
+      'Lord',
+      'Miss',
       'Mr',
       'Mrs',
       'Ms',
-      'Miss',
-      'Dr',
+      'Prince',
+      'Princess'
       'Professor',
+      'Queen',
+      'Reverend',
+      'Saint',
       'Sir',
-      'Dame',
-      'Baron',
-      'Baroness',
-      'Lord',
-      'Lady',
     ],
     messages: defineMessages({
+      Admiral: {
+        id: 'option.personTitles.Admiral',
+        defaultMessage: 'Admiral',
+      },
+      Baron: {
+        id: 'option.personTitles.Baron',
+        defaultMessage: 'Baron',
+      },
+      Baroness: {
+        id: 'option.personTitles.Baroness',
+        defaultMessage: 'Baroness',
+      },
+      Captain: {
+        id: 'option.personTitles.Captain',
+        defaultMessage: 'Captain',
+      },
+      Commander: {
+        id: 'option.personTitles.Commander',
+        defaultMessage: 'Commander',
+      },
+      Commodore: {
+        id: 'option.personTitles.Commodore',
+        defaultMessage: 'Commodore',
+      },
+      Count: {
+        id: 'option.personTitles.Count',
+        defaultMessage: 'Count',
+      },
+      Countess: {
+        id: 'option.personTitles.Countess',
+        defaultMessage: 'Countess',
+      },
+      Dame: {
+        id: 'option.personTitles.Dame',
+        defaultMessage: 'Dame',
+      },
+      Dr: {
+        id: 'option.personTitles.Dr',
+        defaultMessage: 'Dr',
+      },
+      General: {
+        id: 'option.personTitles.General',
+        defaultMessage: 'General',
+      },
+      Governor: {
+        id: 'option.personTitles.Governor',
+        defaultMessage: 'Governor',
+      },
+      Honorable: {
+        id: 'option.personTitles.Honorable',
+        defaultMessage: 'Honorable',
+      },
+      Judge: {
+        id: 'option.personTitles.Judge',
+        defaultMessage: 'Judge',
+      },
+      King: {
+        id: 'option.personTitles.King',
+        defaultMessage: 'King',
+      },
+      Lady: {
+        id: 'option.personTitles.Lady',
+        defaultMessage: 'Lady',
+      },
+      Lord: {
+        id: 'option.personTitles.Lord',
+        defaultMessage: 'Lord',
+      },
+      Miss: {
+        id: 'option.personTitles.Miss',
+        defaultMessage: 'Miss',
+      },
       Mr: {
         id: 'option.personTitles.Mr',
         defaultMessage: 'Mr',
@@ -97,41 +185,33 @@ export default {
         id: 'option.personTitles.Ms',
         defaultMessage: 'Ms',
       },
-      Miss: {
-        id: 'option.personTitles.Miss',
-        defaultMessage: 'Miss',
+      Prince: {
+        id: 'option.personTitles.Prince',
+        defaultMessage: 'Prince',
       },
-      Dr: {
-        id: 'option.personTitles.Dr',
-        defaultMessage: 'Dr',
+      Princess: {
+        id: 'option.personTitles.Princess',
+        defaultMessage: 'Princess',
       },
       Professor: {
         id: 'option.personTitles.Professor',
         defaultMessage: 'Professor',
       },
+      Queen: {
+        id: 'option.personTitles.Queen',
+        defaultMessage: 'Queen',
+      },
+      Reverend: {
+        id: 'option.personTitles.Reverend',
+        defaultMessage: 'Reverend',
+      },
+      Saint: {
+        id: 'option.personTitles.Saint',
+        defaultMessage: 'Saint',
+      },
       Sir: {
         id: 'option.personTitles.Sir',
         defaultMessage: 'Sir',
-      },
-      Dame: {
-        id: 'option.personTitles.Dame',
-        defaultMessage: 'Dame',
-      },
-      Baron: {
-        id: 'option.personTitles.Baron',
-        defaultMessage: 'Baron',
-      },
-      Baroness: {
-        id: 'option.personTitles.Baroness',
-        defaultMessage: 'Baroness',
-      },
-      Lord: {
-        id: 'option.personTitles.Lord',
-        defaultMessage: 'Lord',
-      },
-      Lady: {
-        id: 'option.personTitles.Lady',
-        defaultMessage: 'Lady',
       },
     }),
   },
