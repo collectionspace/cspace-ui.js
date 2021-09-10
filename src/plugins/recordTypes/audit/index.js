d8ecec63aac5417f9f4b4b53a76a4ac0507fcf8b
Michael Ritter <emericaskate15@gmail.com>
2021-09-10 15:37:44 -0600

    Create fields for audit records

--- src/plugins/recordTypes/audit/index.js
+++ src/plugins/recordTypes/audit/index.js
@@ -1,10 +1,12 @@
 import columns from './columns';
+import fields from './fields';
 import messages from './messages';
 
 export default () => (configContext) => ({
   recordTypes: {
     audit: {
       messages,
+      fields: fields(configContext),
       columns: columns(configContext),
     },
   },
