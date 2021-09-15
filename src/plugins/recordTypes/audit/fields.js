2b157989f6fcff02d2eca77d4efa40adfcc107ad
Michael Ritter <emericaskate15@gmail.com>
2021-09-15 13:36:03 -0600

    Add audit fields for displaying the full record

--- src/plugins/recordTypes/audit/fields.js
+++ src/plugins/recordTypes/audit/fields.js
@@ -2,6 +2,10 @@ import { defineMessages } from 'react-intl';
 
 export default (configContext) => {
   const {
+    CompoundInput,
+  } = configContext.inputComponents;
+
+  const {
     DATA_TYPE_DATETIME,
   } = configContext.dataTypes;
 
@@ -56,6 +60,26 @@ export default (configContext) => {
           }),
         },
       },
+      saveMessage: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.audit_common.saveMessage.name',
+              defaultMessage: 'Save message',
+            },
+          }),
+        },
+      },
+      eventComment: {
+        [config]: {
+          messages: {
+            name: {
+              id: 'field.audit_common.eventComment.name',
+              defaultMessage: 'Event comment',
+            },
+          },
+        },
+      },
       eventType: {
         [config]: {
           messages: defineMessages({
@@ -85,6 +109,73 @@ export default (configContext) => {
               defaultMessage: 'Event date',
             },
           }),
+        },
+      },
+      fieldChangedGroupList: {
+        [config]: {
+          view: {
+            type: CompoundInput,
+          },
+        },
+        fieldChangedGroup: {
+          [config]: {
+            messages: defineMessages({
+              name: {
+                id: 'field.audit_common.fieldChangedGroup.name',
+                defaultMessage: 'Field changed',
+              },
+            }),
+          },
+          key: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.audit_common.key.name',
+                  defaultMessage: 'Key',
+                },
+              }),
+            },
+          },
+          fieldName: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.audit_common.fieldName.name',
+                  defaultMessage: 'Field name',
+                },
+              }),
+            },
+          },
+          originalValue: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.audit_common.originalValue.name',
+                  defaultMessage: 'Original value',
+                },
+              }),
+            },
+          },
+          newValue: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.audit_common.newValue.name',
+                  defaultMessage: 'New value',
+                },
+              }),
+            },
+          },
+          changeReason: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.audit_common.changeReason.name',
+                  defaultMessage: 'Change reason',
+                },
+              }),
+            },
+          },
         },
       },
     },
