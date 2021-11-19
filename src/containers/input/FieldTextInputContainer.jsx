546b7e9479cea4298504a3fdfd250f8d70124d6d
Michael Ritter <emericaskate15@gmail.com>
2021-11-19 15:20:16 -0700

    Start to handle formatting lists, add merge props

--- src/containers/input/FieldTextInputContainer.jsx
+++ src/containers/input/FieldTextInputContainer.jsx
@@ -17,7 +17,20 @@ const propTypes = {
 };
 
 export const formatHumanReadable = (type, value, context) => {
-  const formatted = formatRecordTypeSourceField(type, value, context);
+  let formatted;
+
+  // the key is created with schema:fieldName:listIndex
+  // we have 3 outcomes -- an array of index 1 (unqualified), 2 (qualified), or 3 (qualified list)
+  // see: AuditDocumentHandler.java
+  const parts = value.split(':');
+  if (parts.length === 2) {
+    formatted = formatRecordTypeSourceField(type, value, context);
+  } else if (parts.length === 3) {
+    // todo: get child of the key
+    formatted = formatRecordTypeSourceField(type, `${parts[0]}:${parts[1]}`, context);
+  } else {
+    formatted = value;
+  }
 
   // when the fieldConfig isn't found, the return is of the form [${fieldName}], which is
   // less readable than the value itself
@@ -34,6 +47,7 @@ const mapStateToProps = (state, ownProps) => {
 
   let formattedValue;
   const recordData = getRecordData(state, csid);
+
   if (recordData && value) {
     const auditedType = recordData.getIn(['ns3:audit_common', 'resourceType']).toLowerCase();
     formattedValue = formatHumanReadable(auditedType, value, { intl, config });
@@ -44,7 +58,24 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export const ConnectedFieldTextContainer = connect(mapStateToProps)(TextInput);
+/**
+ * trim out props not needed for TextInput
+ */
+const mergeProps = (stateProps, _dispatchProps, ownProps) => {
+  const {
+    intl,
+    csid,
+    config,
+    ...remainingProps
+  } = ownProps;
+
+  return {
+    ...stateProps,
+    ...remainingProps,
+  };
+};
+
+export const ConnectedFieldTextContainer = connect(mapStateToProps, null, mergeProps)(TextInput);
 
 const EnhancedFieldTextContainer = injectIntl(withConfig(withCsid(ConnectedFieldTextContainer)));
 
