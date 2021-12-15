9be15343ef00e5f928cf4108265683ee840f0cce
Michael Ritter <emericaskate15@gmail.com>
2021-12-15 16:09:00 -0700

    Formatting for qualified lists

--- src/containers/input/FieldTextInputContainer.jsx
+++ src/containers/input/FieldTextInputContainer.jsx
@@ -2,6 +2,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { components as inputComponents } from 'cspace-input';
 import { injectIntl, intlShape } from 'react-intl';
+import get from 'lodash/get';
+import { NS_PREFIX } from '../../constants/xmlNames';
+import { configKey } from '../../helpers/configHelpers';
 import { formatRecordTypeSourceField } from '../../helpers/formatHelpers';
 import withConfig from '../../enhancers/withConfig';
 import withCsid from '../../enhancers/withCsid';
@@ -16,18 +19,37 @@ const propTypes = {
   value: PropTypes.string,
 };
 
+export const getFirstChild = (recordType, partName, fieldName, { config }) => {
+  const recordTypeConfig = config.recordTypes[recordType];
+  const partDescriptor = get(recordTypeConfig, ['fields', 'document', `${NS_PREFIX}:${partName}`]);
+  if (!partDescriptor) {
+    return fieldName;
+  }
+
+  let updatedField = fieldName;
+  const fieldDescriptor = partDescriptor[fieldName];
+  if (fieldDescriptor) {
+    updatedField = Object.keys(fieldDescriptor).filter((key) => key !== configKey);
+  }
+
+  return updatedField;
+};
+
 export const formatHumanReadable = (type, value, context) => {
   let formatted;
 
   // the key is created with schema:fieldName:listIndex
   // we have 3 outcomes -- an array of index 1 (unqualified), 2 (qualified), or 3 (qualified list)
+  // for qualified lists, use the child key if available
   // see: AuditDocumentHandler.java
   const parts = value.split(':');
+  const [partName, fieldName] = parts;
   if (parts.length === 2) {
     formatted = formatRecordTypeSourceField(type, value, context);
   } else if (parts.length === 3) {
-    // todo: get child of the key
-    formatted = formatRecordTypeSourceField(type, `${parts[0]}:${parts[1]}`, context);
+    // todo: display index?
+    const childName = getFirstChild(type, partName, fieldName, context);
+    formatted = formatRecordTypeSourceField(type, `${partName}:${childName}`, context);
   } else {
     formatted = value;
   }
