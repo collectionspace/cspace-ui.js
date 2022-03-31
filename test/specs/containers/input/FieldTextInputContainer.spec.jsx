7ee5df3da0ba0210073e579ee6ef606560579c0c
Michael Ritter <emericaskate15@gmail.com>
2022-03-31 15:32:10 -0600

    Add test to cover other cases

--- test/specs/containers/input/FieldTextInputContainer.spec.jsx
+++ test/specs/containers/input/FieldTextInputContainer.spec.jsx
@@ -18,12 +18,51 @@ const intl = {
 const recordType = 'collectionobject';
 
 const objectNumberConfig = {
-  messages: defineMessages({
-    name: {
-      id: 'field.collectionobjects_common.objectNumber.name',
-      defaultMessage: 'Identification number',
+  [configKey]: {
+    messages: defineMessages({
+      name: {
+        id: 'field.collectionobjects_common.objectNumber.name',
+        defaultMessage: 'Identification number',
+      },
+    }),
+  },
+};
+
+const commentsConfig = {
+  comment: {
+    [configKey]: {
+      messages: defineMessages({
+        name: {
+          id: 'field.collectionobjects_common.comment.name',
+          defaultMessage: 'Comment',
+        },
+      }),
     },
-  }),
+  },
+};
+
+const titleGroupListConfig = {
+  titleGroup: {
+    [configKey]: {
+      messages: defineMessages({
+        name: {
+          id: 'field.collectionobjects_common.titleGroup.name',
+          defaultMessage: 'Title',
+        },
+      }),
+
+    },
+    titleLanguage: {
+      [configKey]: {
+        messages: defineMessages({
+          name: {
+            id: 'field.collectionobjects_common.titleLanguage.name',
+            defaultMessage: 'Title language',
+          },
+        }),
+      },
+    },
+  },
 };
 
 const config = {
@@ -32,9 +71,9 @@ const config = {
       fields: {
         document: {
           'ns2:collectionobjects_common': {
-            objectNumber: {
-              [configKey]: objectNumberConfig,
-            },
+            objectNumber: objectNumberConfig,
+            comments: commentsConfig,
+            titleGroupList: titleGroupListConfig,
           },
         },
       },
@@ -43,10 +82,29 @@ const config = {
 };
 
 describe('FieldTextInputContainer', () => {
-  it('should return human readable input when available', () => {
+  it('should return the default message for fields', () => {
     const field = 'collectionobjects_common:objectNumber';
     const formatted = formatHumanReadable(recordType, field, { intl, config });
     formatted.should.equal('Identification number');
+  });
+
+  it('should return the child message for lists', () => {
+    const field = 'collectionobjects_common:comments:03E8';
+    const formatted = formatHumanReadable(recordType, field, { intl, config });
+    formatted.should.equal('Comment');
+  });
+
+  it('should return the group message for GroupLists', () => {
+    const field = 'collectionobjects_common:titleGroupList';
+    const formatted = formatHumanReadable(recordType, field, { intl, config });
+    formatted.should.equal('Title');
+  });
+
+  it('should return the group, index, and item for GroupList items', () => {
+    const field = 'collectionobjects_common:titleGroupList/0/titleLanguage';
+    const formatted = formatHumanReadable(recordType, field, { intl, config });
+    // would be nice to have an actual intl fill this in
+    formatted.should.equal('{groupList} #{index} — {listItem}');
   });
 
   it('should return the field when formatting is not available', () => {
