76b5d495ac9b04bb9e83309978857682aaa3a46b
Michael Ritter <mikejritter@users.noreply.github.com>
2023-04-19 21:03:58 -0600

    DRYD-1133: Add controlled chronology fields to collectionobject (#177)
    Co-authored-by: Ray Lee <ray.lee@lyrasis.org>

--- src/plugins/recordTypes/collectionobject/messages.js
+++ src/plugins/recordTypes/collectionobject/messages.js
@@ -92,6 +92,10 @@ export default {
       id: 'inputTable.collectionobject.assocEvent',
       defaultMessage: 'Associated event',
     },
+    assocControlledEvent: {
+      id: 'inputTable.collectionobject.assocControlledEvent',
+      defaultMessage: 'Associated controlled event',
+    },
     ownershipExchange: {
       id: 'inputTable.collectionobject.ownershipExchange',
       defaultMessage: 'Ownership exchange',
