bdfeda997bc57f30e3c097b8c31d46c22fb8960c
Michael Ritter <emericaskate15@gmail.com>
2024-04-04 14:51:17 -0600

    Add 'Welcome' to the page title

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { defineMessages, injectIntl, intlShape } from 'react-intl';
 import About from '../sections/About';
 import LoginForm from '../login/LoginForm';
 import styles from '../../../styles/cspace-ui/LoginPage.css';
 
-export default function LoginPage() {
-  return (
-    <div className={styles.common}>
-      <div className={styles.about}>
-        <About />
-      </div>
+const propTypes = {
+  intl: intlShape.isRequired,
+};
 
-      <div className={styles.login}>
-        <LoginForm />
+const messages = defineMessages({
+  title: {
+    id: 'loginPage.title',
+    description: 'Title of the login page.',
+    defaultMessage: 'Welcome',
+  },
+});
+
+function LoginPage(props) {
+  const {
+    intl,
+  } = props;
+
+  return (
+    <>
+      <Helmet>
+        <title>{intl.formatMessage(messages.title)}</title>
+      </Helmet>
+      <div className={styles.common}>
+        <div className={styles.about}>
+          <About />
+        </div>
+
+        <div className={styles.login}>
+          <LoginForm />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+LoginPage.propTypes = propTypes;
+
+export default injectIntl(LoginPage);
