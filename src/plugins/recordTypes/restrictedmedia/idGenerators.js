2f77e036a6ae7c489189523bbdaa295fa72a5ec4
Michael Ritter <emericaskate15@gmail.com>
2024-07-15 13:04:19 -0600

    Updated restrictedmedia id generator

--- src/plugins/recordTypes/restrictedmedia/idGenerators.js
+++ src/plugins/recordTypes/restrictedmedia/idGenerators.js
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default {
   restrictedmedia: {
-    csid: 'cd91d8b8-f346-4925-a425-93e02bd1c5c9',
+    csid: '17772cbd-8de1-4969-ac2e-c51798bc4e75',
     messages: defineMessages({
       type: {
         id: 'idGenerator.restrictedmedia.type',
