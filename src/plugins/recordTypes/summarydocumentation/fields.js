1ef49edfca0a6bec782a6770eafdec037b9cd325
Michael Ritter <mike.ritter@lyrasis.org>
2024-08-21 13:39:01 -0600

    Add archculture source

--- src/plugins/recordTypes/summarydocumentation/fields.js
+++ src/plugins/recordTypes/summarydocumentation/fields.js
@@ -686,7 +686,7 @@ export default (configContext) => {
                 view: {
                   type: AutocompleteInput,
                   props: {
-                    source: 'concept/ethculture',
+                    source: 'concept/ethculture,concept/archculture',
                   },
                 },
               },
