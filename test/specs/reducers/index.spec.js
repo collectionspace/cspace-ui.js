554606c7992bae45574bc435288def2b25945240
Michael Ritter <mike.ritter@lyrasis.org>
2024-11-18 13:46:52 -0700

    Add test for getTagsForProcedure selector

--- test/specs/reducers/index.spec.js
+++ test/specs/reducers/index.spec.js
@@ -61,6 +61,7 @@ import reducer, {
   getSearchToSelectVocabulary,
   getNotifications,
   getOpenModalName,
+  getTagsForRecord,
 } from '../../../src/reducers';
 
 import { searchKey } from '../../../src/reducers/search';
@@ -1025,6 +1026,19 @@ describe('reducer', () => {
           modal: modalName,
         }),
       }).should.equal(modalName);
+    });
+  });
+
+  describe('getTagsForProcedure selector', () => {
+    it('should select from the tags key', () => {
+      const recordType = 'collectionobject';
+      const recordTags = Immutable.Map();
+
+      getTagsForRecord({
+        tags: Immutable.Map({
+          [recordType]: recordTags,
+        }),
+      }, recordType).should.deep.equal(recordTags);
     });
   });
 });
