98fb4bdd9ab44711f5330e27d08d8cc2aa0829fe
spiros dimopulos <spiros@wantedpixel.com>
2025-06-13 07:31:31 +0300

    NOJIRA: added explanatory comment

--- src/index.jsx
+++ src/index.jsx
@@ -132,6 +132,7 @@ export default (uiConfig) => {
 
     render(<AppContainer {...props} />, mountNode);
 
+    // HMR related: https://webpack.js.org/concepts/hot-module-replacement/
     if (module.hot) {
       module.hot.accept('./containers/AppContainer', () => {
         render(<AppContainer {...props} />, mountNode);
