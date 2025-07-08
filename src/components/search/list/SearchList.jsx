f78ba2b3cd87295fcacf228477a5b8f6411409d6
Michael Ritter <mike.ritter@lyrasis.org>
2025-07-08 13:59:18 -0600

    Read search results from redux

--- src/components/search/list/SearchList.jsx
+++ src/components/search/list/SearchList.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './SearchList.css';
 import deactivate from '../../../../images/deactivate.svg';
+import { getSearchResult } from '../../../reducers';
+import { NEW_SEARCH_RESULT_PAGE_SEARCH_NAME } from '../../../constants/searchNames';
 
-export function DetailItem() {
+export function DetailItem({ result }) {
   // todo: read fields from record config
 
   return (
@@ -10,20 +13,24 @@ export function DetailItem() {
       <img src={deactivate} className={styles.detailimg} />
       <input style={{ alignSelf: 'flex-start' }} type="checkbox" />
       <ol>
-        <li>ID: 2025.1.2</li>
-        <li>Title: Published Item Title Test</li>
-        <li>Responsible Department: Departmentalized</li>
-        <li>Current Location: Storage Site A</li>
-        <li>Brief Description: Some information about this item...</li>
+        <li>{`ID: ${result.objectNumber}`}</li>
+        <li>{`Title: ${result.title}`}</li>
+        <li>{`Responsible Department: ${result.responsibleDepartment}`}</li>
+        <li>{`Current Location: ${result.computedCurrentLocation}`}</li>
+        <li>{`Brief Description: ${result.briefDescription}`}</li>
       </ol>
     </div>
   );
 }
 
-export default function SearchDetailList() {
+export default function SearchDetailList({ searchDescriptor }) {
+  const results = useSelector((state) => getSearchResult(state,
+    NEW_SEARCH_RESULT_PAGE_SEARCH_NAME,
+    searchDescriptor));
+
   return (
     <div className={styles.detail}>
-      <DetailItem />
+      {results.map((result) => <DetailItem result={result} />)}
     </div>
   );
 }
