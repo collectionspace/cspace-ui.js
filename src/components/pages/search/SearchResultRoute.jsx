0aedf125dea6c80ff98490d1d2313c43bdc27041
Michael Ritter <mike.ritter@lyrasis.org>
2025-07-08 13:50:58 -0600

    Add function to help navigate between new/old search results

---
+++ src/components/pages/search/SearchResultRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import get from 'lodash/get';
+import SearchResults from './SearchResults';
+import SearchResultPageContainer from '../../../containers/pages/SearchResultPageContainer';
+import { useConfig } from '../../config/ConfigProvider';
+
+/**
+ * A function which chooses either the new search result page (SearchResults) or
+ * old search result page (SearchResultPageContainer) for rendering
+ *
+ * @param {*} props
+ * @returns the component to render
+ */
+export default function SearchResultRoute(props) {
+  const { recordType } = useParams();
+  const config = useConfig();
+
+  const isNewSearch = get(config, ['recordTypes', recordType, 'serviceConfig', 'useUpdatedSearch']);
+
+  return isNewSearch
+    ? <SearchResults {...props} />
+    : <SearchResultPageContainer {...props} />;
+}
