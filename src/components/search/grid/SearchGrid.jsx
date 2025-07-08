f00e054e4ca8f8866d0681032e739ef214a7da06
Michael Ritter <mike.ritter@lyrasis.org>
2025-07-08 13:59:18 -0600

    Read search results from redux

--- src/components/search/grid/SearchGrid.jsx
+++ src/components/search/grid/SearchGrid.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './SearchGrid.css';
 import deactivate from '../../../../images/deactivate.svg';
+import { getSearchResult } from '../../../reducers';
+import { NEW_SEARCH_RESULT_PAGE_SEARCH_NAME } from '../../../constants/searchNames';
 
-export function SimpleCard() {
+export function SimpleCard({ result }) {
   // todo: read fields from record config
+  // todo: loading image
   return (
     <div>
       <img src={deactivate} className={styles.card} />
       <div>
         <div className={styles.summary}>
           <input type="checkbox" />
-          <span>2025.1.2: Object Test</span>
+          <span>{`${result.objectName} : ${result.title}`}</span>
         </div>
-        <span>6/6/2025, 12:41 PM</span>
+        <span>{result.updatedAt}</span>
       </div>
     </div>
   );
 }
 
-export default function SearchResultGrid() {
+export default function SearchResultGrid({ searchDescriptor }) {
+  const results = useSelector((state) => getSearchResult(state,
+    NEW_SEARCH_RESULT_PAGE_SEARCH_NAME,
+    searchDescriptor));
+
   // todo: sidebar is open prop to control grid size?
   // or could try flexbox
 
   return (
     <div className={styles.grid}>
-      <SimpleCard />
-      <SimpleCard />
-      <SimpleCard />
-      <SimpleCard />
-      <SimpleCard />
-      <SimpleCard />
+      {results.map((result) => <SimpleCard result={result} />)}
     </div>
   );
 }
