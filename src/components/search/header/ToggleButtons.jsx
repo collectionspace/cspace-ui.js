5397b33dec09969a6eea145230f7839a593da2d0
Michael Ritter <mike.ritter@lyrasis.org>
2025-07-18 12:16:58 -0600

    ToggleButton wip

---
+++ src/components/search/header/ToggleButtons.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { components as inputComponents } from 'cspace-input';
+import PropTypes from 'prop-types';
+import styles from '../../../../styles/cspace-ui/ButtonBar.css';
+
+const { Button } = inputComponents;
+
+const toggleButtonProps = {
+  label: PropTypes.string,
+  style: PropTypes.any,
+  name: PropTypes.string,
+};
+
+export function ToggleButton(props) {
+  const {
+    label,
+    style,
+    name,
+    ...remainingProps
+  } = props;
+
+  return (
+    <Button
+      className={style}
+      icon
+      name={name}
+      {...remainingProps}
+    >
+      {label}
+    </Button>
+  );
+}
+
+ToggleButton.propTypes = toggleButtonProps;
+
+const propTypes = {
+  items: PropTypes.array,
+  renderButton: PropTypes.func,
+};
+
+/**
+ * todo: figure out styling
+ *
+ * also a side note - we might want this to be part of the
+ * select bar
+ *
+ * @param {*} param0
+ * @returns
+ */
+export function ToggleButtonContainer({ items, renderButton }) {
+  const toggleStyles = {
+    flexBasis: 'calc(3/5 * 100%)',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  };
+
+  return (
+    <div className={styles.common} style={toggleStyles}>
+      {items.map((item) => renderButton(item))}
+    </div>
+  );
+}
+
+ToggleButtonContainer.propTypes = propTypes;
