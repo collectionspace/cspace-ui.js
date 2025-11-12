50df54496207a64cb24a1d555a76e96737f6504c
Michael Ritter <mike.ritter@lyrasis.org>
2025-11-12 14:04:20 -0700

    Make DetailItem its own component

---
+++ src/components/search/list/DetailItem.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Immutable from 'immutable';
+import { FormattedMessage } from 'react-intl';
+import { components as inputComponents } from 'cspace-input';
+import { useConfig } from '../../config/ConfigProvider';
+import BlobImage from '../../media/BlobImage';
+import { setResultItemSelected } from '../../../actions/search';
+import { SEARCH_RESULT_PAGE_SEARCH_NAME } from '../../../constants/searchNames';
+import { CheckboxInput } from '../../../helpers/configContextInputs';
+import styles from '../../../../styles/cspace-ui/SearchList.css';
+
+const { Button } = inputComponents;
+
+const propTypes = {
+  item: PropTypes.instanceOf(Immutable.Map),
+  index: PropTypes.number,
+  detailConfig: PropTypes.object,
+  searchDescriptor: PropTypes.object,
+  listType: PropTypes.string,
+  selectedItems: PropTypes.instanceOf(Immutable.Map),
+};
+
+const renderEditButton = (location, state) => {
+  const button = (
+    <Button name="edit">
+      <FormattedMessage
+        id="searchDetailList.editLabel"
+        description="Label of the edit record button."
+        defaultMessage="Edit Record"
+      />
+    </Button>
+  );
+  return location ? (
+    <Link to={{ pathname: location, state }}>
+      {button}
+    </Link>
+  ) : button;
+};
+
+export default function DetailItem({
+  item, index, detailConfig, searchDescriptor, listType, selectedItems,
+}) {
+  const config = useConfig();
+  const dispatch = useDispatch();
+
+  const {
+    title: {
+      formatter: titleFormatter,
+    },
+    subtitle: {
+      formatter: subtitleFormatter,
+    },
+    description: {
+      formatter: descriptionFormatter,
+    },
+    tags: {
+      formatter: tagFormatter,
+    },
+    footer: {
+      formatter: footerFormatter,
+    },
+    aside: {
+      formatter: asideFormatter,
+    },
+  } = detailConfig;
+
+  const csid = item.get('csid');
+  const blobCsid = item.get('blobCsid');
+  const selected = selectedItems ? selectedItems.has(csid) : false;
+
+  const listTypeConfig = config.listTypes[listType];
+  const { getItemLocationPath } = listTypeConfig;
+  let location;
+  let state;
+  if (getItemLocationPath) {
+    location = getItemLocationPath(item, { config, searchDescriptor });
+    state = {
+      searchDescriptor: searchDescriptor.toJS(),
+      // The search traverser on records will always link to the search result page, so use
+      // its search name.
+      searchName: SEARCH_RESULT_PAGE_SEARCH_NAME,
+    };
+  }
+
+  const renderInfo = () => <aside>{asideFormatter(item)}</aside>;
+  const renderDescriptionBlock = () => (
+    <div className={styles.description}>
+      {titleFormatter(item)}
+      {subtitleFormatter(item)}
+      {descriptionFormatter(item)}
+      {tagFormatter(item)}
+      {footerFormatter(item)}
+    </div>
+  );
+
+  const editButton = renderEditButton(location, state);
+  // todo: NoBlobFound image
+  const blob = blobCsid ? <BlobImage csid={blobCsid} derivative="Small" className={styles.detailImg} /> : null;
+  return (
+    <div className={styles.innerDetail}>
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      {blob}
+      <CheckboxInput
+        embedded
+        className={styles.detailCheckbox}
+        name={`${index}`}
+        value={selected}
+        onCommit={(path, value) => dispatch(setResultItemSelected(config,
+          SEARCH_RESULT_PAGE_SEARCH_NAME,
+          searchDescriptor,
+          listType,
+          parseInt(path[0], 10),
+          value))}
+        onClick={(event) => event.stopPropagation()}
+      />
+      {renderDescriptionBlock()}
+      <div className={styles.info}>
+        {renderInfo()}
+        {editButton}
+      </div>
+    </div>
+  );
+}
+
+DetailItem.propTypes = propTypes;
