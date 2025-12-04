d8a99bd76a346ae942cfe1151bd21f0500778ba8
Michael Ritter <mike.ritter@lyrasis.org>
2025-12-04 16:37:10 -0700

    Shared component for thumbnails

--- src/components/search/SearchResultImage.jsx
+++ src/components/search/SearchResultImage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { defineMessages, intlShape } from 'react-intl';
+import { defineMessages, injectIntl, intlShape } from 'react-intl';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import PropTypes from 'prop-types';
 import BlobImage from '../media/BlobImage';
-import { SEARCH_RESULT_PAGE_SEARCH_NAME } from '../../constants/searchNames';
-import { useConfig } from '../config/ConfigProvider';
 
 const messages = defineMessages({
   alt: {
@@ -15,38 +13,18 @@ const messages = defineMessages({
 });
 
 /**
- * A wrapper around BlobImage which has an anchor if available with default alt text
+ * A wrapper around BlobImage which has navigation on click if available and alt text
  */
-export default function SearchResultImage(props) {
-  const {
-    derivative,
-    intl,
-    item,
-    getItemLocationPath,
-    searchDescriptor,
-  } = props;
-
-  const config = useConfig();
-
-  let location;
-  let state;
-  if (getItemLocationPath) {
-    location = getItemLocationPath(item, { config, searchDescriptor });
-    state = {
-      searchDescriptor: searchDescriptor.toJS(),
-      // The search traverser on records will always link to the search result page, so use
-      // its search name.
-      searchName: SEARCH_RESULT_PAGE_SEARCH_NAME,
-    };
-  }
-
+function SearchResultImage({
+  derivative, intl, item, location, state,
+}) {
   const itemCsid = item.get('csid');
   const blobCsid = item.get('blobCsid');
   const blobAlt = item.get('blobAltText');
   const altText = blobAlt || intl.formatMessage(messages.alt, { itemCsid });
 
   const blob = <BlobImage csid={blobCsid} derivative={derivative} alt={altText} />;
-  return location ? (
+  return location && state ? (
     <Link to={{ pathname: location, state }}>
       {blob}
     </Link>
@@ -57,6 +35,8 @@ SearchResultImage.propTypes = {
   derivative: PropTypes.string,
   intl: intlShape,
   item: PropTypes.object,
-  getItemLocationPath: PropTypes.func,
-  searchDescriptor: PropTypes.object,
+  location: PropTypes.string,
+  state: PropTypes.object,
 };
+
+export default injectIntl(SearchResultImage);
