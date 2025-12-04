4f603a2403be4190f2a9877cf6f31366ff1c6a54
Michael Ritter <mike.ritter@lyrasis.org>
2025-12-04 16:37:46 -0700

    Refactor for new config + formatters

--- src/components/search/grid/SearchResultGrid.jsx
+++ src/components/search/grid/SearchResultGrid.jsx
@@ -2,46 +2,71 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 import { useSelector } from 'react-redux';
-import { injectIntl } from 'react-intl';
 import { get } from 'lodash';
+import { Link } from 'react-router-dom';
 import { readListItems } from '../searchResultHelpers';
 import { getSearchResult, getSearchSelectedItems } from '../../../reducers';
 import { SEARCH_RESULT_PAGE_SEARCH_NAME } from '../../../constants/searchNames';
 import { useConfig } from '../../config/ConfigProvider';
 import styles from '../../../../styles/cspace-ui/SearchGrid.css';
 import { getListTypeFromResult } from '../../../helpers/searchHelpers';
-import BlobImage from '../../media/BlobImage';
 import SearchResultCheckbox from '../SearchResultCheckbox';
-import { formatRefName } from '../../../helpers/formatHelpers';
-
-const GRID_COLUMN_SET = 'grid';
+import SearchResultImage from '../SearchResultImage';
 
 const cardPropTypes = {
-  result: PropTypes.instanceOf(Immutable.Map),
+  item: PropTypes.instanceOf(Immutable.Map),
   index: PropTypes.number,
   listType: PropTypes.string,
-  titleFields: PropTypes.array,
-  subtitleFields: PropTypes.array,
+  gridConfig: PropTypes.shape({
+    title: PropTypes.shape({
+      formatter: PropTypes.func,
+    }),
+    subtitle: PropTypes.shape({
+      formatter: PropTypes.func,
+    }),
+    description: PropTypes.shape({
+      formatter: PropTypes.func,
+    }),
+    tags: PropTypes.shape({
+      formatter: PropTypes.func,
+    }),
+  }),
   searchDescriptor: PropTypes.object,
 };
 
 export function SearchResultCard({
-  result, index, titleFields, subtitleFields, searchDescriptor, listType,
+  item, gridConfig, index, searchDescriptor, listType,
 }) {
   const config = useConfig();
 
-  const csid = result.get('csid');
-  const blobCsid = result.get('blobCsid');
+  const {
+    description: {
+      formatter: descriptionFormatter,
+    } = {},
+    tags: {
+      formatter: tagFormatter,
+    } = {},
+    title: {
+      formatter: titleFormatter,
+    } = {},
+    subtitle: {
+      formatter: subtitleFormatter,
+    } = {},
+  } = gridConfig;
+
   const selectedItems = useSelector((state) => getSearchSelectedItems(state,
     SEARCH_RESULT_PAGE_SEARCH_NAME));
+
+  const csid = item.get('csid');
   const selected = selectedItems ? selectedItems.has(csid) : false;
 
   const listTypeConfig = config.listTypes[listType];
   const { getItemLocationPath } = listTypeConfig;
+
   let location;
   let state;
   if (getItemLocationPath) {
-    location = getItemLocationPath(result, { config, searchDescriptor });
+    location = getItemLocationPath(item, { config, searchDescriptor });
     state = {
       searchDescriptor: searchDescriptor.toJS(),
       // The search traverser on records will always link to the search result page, so use
@@ -50,7 +75,21 @@ export function SearchResultCard({
     };
   }
 
-  const blob = <BlobImage csid={blobCsid} derivative="Medium" />;
+  const blob = (
+    <SearchResultImage
+      derivative="Medium"
+      item={item}
+      location={location}
+      state={state}
+    />
+  );
+
+  const title = location ? (
+    <Link to={{ pathname: location, state }}>
+      {titleFormatter?.(item)}
+    </Link>
+  ) : titleFormatter?.(item);
+
   return (
     <div className={styles.card}>
       {blob}
@@ -63,17 +102,10 @@ export function SearchResultCard({
             selected={selected}
           />
           <div className={styles.info}>
-            <h2>
-              {titleFields.map((field) => field.formatValue(result.get(field.dataKey)))
-                .filter((resultData) => !!resultData)
-                .join(': ')}
-            </h2>
-            <p>
-              {result.get('title') || `${formatRefName(result.get('objectNameControlled'))}, ${result.get('objectName')}`}
-            </p>
-            <p>
-              {`${formatRefName(result.get('agent'))} (${result.get('agentRole')})`}
-            </p>
+            {title}
+            {subtitleFormatter?.(item)}
+            {descriptionFormatter?.(item)}
+            {tagFormatter?.(item)}
           </div>
         </div>
       </div>
@@ -83,18 +115,7 @@ export function SearchResultCard({
 
 SearchResultCard.propTypes = cardPropTypes;
 
-const getGridConfig = (config, searchDescriptor) => {
-  const recordType = searchDescriptor.get('recordType');
-  const subresource = searchDescriptor.get('subresource');
-
-  const configurer = subresource
-    ? config.subresources[subresource]
-    : config.recordTypes[recordType];
-
-  return get(configurer, ['grid']);
-};
-
-function SearchResultGrid({ searchDescriptor, intl }) {
+export default function SearchResultGrid({ searchDescriptor }) {
   const results = useSelector((state) => getSearchResult(state,
     SEARCH_RESULT_PAGE_SEARCH_NAME,
     searchDescriptor));
@@ -106,39 +127,14 @@ function SearchResultGrid({ searchDescriptor, intl }) {
     return null;
   }
 
-  function createFieldConfig(fieldConfig) {
-    return Object.keys(fieldConfig)
-      .filter((name) => !fieldConfig[name].disabled)
-      .sort((nameA, nameB) => {
-        const orderA = fieldConfig[nameA].order;
-        const orderB = fieldConfig[nameB].order;
-        return orderA - orderB;
-      }).map((name) => {
-        const field = fieldConfig[name];
-        return {
-          dataKey: field.dataKey || name,
-          formatValue: (data) => {
-            if (field.formatValue) {
-              const formatterContext = {
-                intl,
-                config,
-                data,
-              };
+  const recordType = searchDescriptor.get('recordType');
+  const subresource = searchDescriptor.get('subresource');
 
-              return field.formatValue(data, formatterContext);
-            }
-            return data;
-          },
-        };
-      });
-  }
+  const configurer = subresource
+    ? config.subresources[subresource]
+    : config.recordTypes[recordType];
 
-  // read headers
-  const cardConfig = getGridConfig(config, searchDescriptor, GRID_COLUMN_SET);
-  const titleConfig = cardConfig.title.fields;
-  const titleFields = createFieldConfig(titleConfig);
-  const subtitleConfig = cardConfig.subtitle.fields;
-  const subtitleFields = createFieldConfig(subtitleConfig);
+  const gridConfig = get(configurer, ['grid']);
 
   return (
     <div className={styles.grid}>
@@ -146,10 +142,9 @@ function SearchResultGrid({ searchDescriptor, intl }) {
         <SearchResultCard
           key={item.get('csid')}
           index={index}
-          result={item}
+          item={item}
           listType={listType}
-          titleFields={titleFields}
-          subtitleFields={subtitleFields}
+          gridConfig={gridConfig}
           searchDescriptor={searchDescriptor}
         />
       ))}
@@ -159,7 +154,4 @@ function SearchResultGrid({ searchDescriptor, intl }) {
 
 SearchResultGrid.propTypes = {
   searchDescriptor: PropTypes.object,
-  intl: PropTypes.object,
 };
-
-export default injectIntl(SearchResultGrid);
