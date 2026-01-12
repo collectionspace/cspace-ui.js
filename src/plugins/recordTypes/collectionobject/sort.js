90e4cd9b558a23aa7e5365cc54157c502fc82835
Michael Ritter <mike.ritter@lyrasis.org>
2026-01-12 16:09:13 -0700

    Add messages and default fields

--- src/plugins/recordTypes/collectionobject/sort.js
+++ src/plugins/recordTypes/collectionobject/sort.js
@@ -1,3 +1,5 @@
+import { defineMessages } from 'react-intl';
+
 /**
  * We store a key which is the value of the sort option, then use the 'sortBy' to store
  * the actual field associated with the key. This is to keep some compatibility with how
@@ -6,24 +8,68 @@
  */
 export default {
   objectNumber: {
+    messages: defineMessages({
+      label: {
+        id: 'sortBy.collectionobjects.objectNumber',
+        defaultMessage: 'Object number',
+      },
+    }),
     sortBy: 'collectionobjects_common:objectNumber',
   },
   objectName: {
+    messages: defineMessages({
+      label: {
+        id: 'sortBy.collectionobjects.objectName',
+        defaultMessage: 'Object name',
+      },
+    }),
     sortBy: 'collectionobjects_common:objectNameList/0/objectName',
   },
   objectNameControlled: {
+    messages: defineMessages({
+      label: {
+        id: 'sortBy.collectionobjects.objectNameControlled',
+        defaultMessage: 'Object name controlled',
+      },
+    }),
     sortBy: 'collectionobjects_common:objectNameList/0/objectNameControlled',
   },
   title: {
+    messages: defineMessages({
+      label: {
+        id: 'sortBy.collectionobjects.title',
+        defaultMessage: 'Title',
+      },
+    }),
     sortBy: 'collectionobjects_common:titleGroupList/0/title',
   },
   updatedAt: {
+    messages: defineMessages({
+      label: {
+        id: 'sortBy.collectionobjects.updatedAt',
+        defaultMessage: 'Updated at',
+      },
+    }),
     sortBy: 'collectionspace_core:updatedAt',
   },
   createdAt: {
+    messages: defineMessages({
+      label: {
+        id: 'sortBy.collectionobjects.createdAt',
+        defaultMessage: 'Created at',
+      },
+    }),
     sortBy: 'collectionspace_core:createdAt',
   },
   computedCurrentLocation: {
+    messages: defineMessages({
+      label: {
+        id: 'sortBy.collectionobjects.computedCurrentLocation',
+        defaultMessage: 'Computed current location',
+      },
+    }),
     sortBy: 'collectionobjects_common:computedCurrentLocation',
   },
+  defaultSortBy: 'updatedAt',
+  defaultSortDirection: 'desc',
 };
