b58fe137bf893264d797ddd30d97cd23c4387128
Michael Ritter <mike.ritter@lyrasis.org>
2026-01-20 13:21:36 -0700

    Update object number message for consistency

--- src/plugins/recordTypes/collectionobject/sort.js
+++ src/plugins/recordTypes/collectionobject/sort.js
@@ -11,7 +11,7 @@ export default {
     messages: defineMessages({
       label: {
         id: 'sortBy.collectionobjects.objectNumber',
-        defaultMessage: 'Object number',
+        defaultMessage: 'Idnetification number',
       },
     }),
     sortBy: 'collectionobjects_common:objectNumber',
